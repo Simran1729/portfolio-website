f42f12b3847a3726ab2c44e94fd855d95534df81
Simran <bhatnagarsimran1306@gmail.com>
2026-06-16 12:28:48 +0530

    removed bottom nav

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -18,7 +18,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
         className="pointer-events-none fixed inset-x-0 bottom-0 z-50 h-[calc(--spacing(24)+env(safe-area-inset-bottom,0))] bg-linear-to-b from-transparent from-[calc(env(safe-area-inset-bottom,0%))] to-background mask-linear-[to_top,var(--background)_25%,transparent] backdrop-blur-[1px]"
         aria-hidden
       />
-      <SiteBottomNav />
+      {/* <SiteBottomNav /> */}
       <ScrollToTop />
     </div>
   )
